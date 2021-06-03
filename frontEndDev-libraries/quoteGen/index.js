67b91a58cfb24ae035eceefdef871280b9958954
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-06-03 16:37:40 +0530

    added fade in animation

--- frontEndDev-libraries/quoteGen/index.js
+++ frontEndDev-libraries/quoteGen/index.js
@@ -1,4 +1,4 @@
-const setBg = () =>{
+const setBg = (element) => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const colorCode = "#" + randomColor;
     document.body.style.backgroundColor = colorCode;
@@ -7,6 +7,7 @@ const setBg = () =>{
         button.style.backgroundColor = colorCode;
     })
     document.getElementById('author').style.color = colorCode;
+    unfade(document.body)
 }
 
 async function getQuotes() {
@@ -18,15 +19,40 @@ async function getQuotes() {
     // .catch(err => {
     //     console.error(err);
     // });
-    let quote=await quoteData.json()
+    let quote = await quoteData.json()
     console.log(quote.quote)
     console.log(quote.author)
-    document.getElementById('text').innerHTML =" "+ quote.quote
+    document.getElementById('text').innerHTML = " " + quote.quote
     document.getElementById('author').innerHTML = "- " + quote.author
-    setBg()
-}
+    setBg(document.getElementById('quote-box'))
 
+}
 getQuotes()
 
 
-document.getElementById('new-quote').addEventListener('click',getQuotes)
+document.getElementById('new-quote').addEventListener('click', getQuotes)
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 20);
+    element.style.display = 'flex';
+    // var op = 0.1;  // initial opacity
+    // var timer = setInterval(function () {
+    //     if (op >= 1) {
+    //         clearInterval(timer);
+    //         element.style.display = 'none';
+    //     }
+    //     element.style.opacity = op;
+    //     element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    //     op += op * 0.1;
+    // }, 50);
+    // element.style.display = 'flex';
+}
